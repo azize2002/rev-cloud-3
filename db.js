d1f17a00351664e097f36255ac3c63a1685b1d66
jihenbo <jihenebo58@gmail.com>
2024-11-20 23:33:08 +0100

    changement fait

--- db.js
+++ db.js
@@ -1,27 +1,24 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require('sequelize');
 
-// database
+// Configuration de la base de données
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
-  {
-    host: '', // Host
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    define: {
-      createdAt: 'added',
-      updatedAt: 'updated',
+    'rev-cloud-3', // Nom de la base de données
+    'mahmoud',     // Nom d'utilisateur
+    '<motdepasse>', // Mot de passe (remplacez par le vôtre)
+    {
+        host: 'localhost', // Remplacez par l'hôte de votre base de données
+        dialect: 'postgres', // Type de base de données
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+        define: {
+            createdAt: 'added',
+            updatedAt: 'updated',
+        },
     }
-  },
-)
+);
 
-sequelize.authenticate()
-sequelize.sync()
-
-module.exports = sequelize
+module.exports = sequelize;
